17896c82ac31b903fdc21766e4fd40af96e6014f
Travis Bennett <tandrewbennett@hotmail.com>
2017-01-23 00:11:24 +0000

    Update the name of the stapler package config interface.

--- src/IlluminateConfig.php
+++ src/IlluminateConfig.php
@@ -2,10 +2,10 @@
 
 namespace Codesleeve\LaravelStapler;
 
-use Codesleeve\Stapler\Interfaces\Config;
+use Codesleeve\Stapler\Interfaces\ConfigInterface;
 use Illuminate\Contracts\Config\Repository;
 
-class IlluminateConfig implements Config
+class IlluminateConfig implements ConfigInterface
 {
     /**
      * An instance of Laravel's config class.
